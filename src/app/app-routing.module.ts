3de30c231bdebeb3e6240597052a263f8a99361c
Selim <zouache.selim57@gmail.com>
2022-10-25 15:37:15 +0200

    new commit add all genres movies in english part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'movies',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
+  ///en
   {
     path: 'movies',
     loadChildren: () => import('./pages/languages/english/movies/movies.module').then( m => m.MoviesPageModule)
@@ -37,6 +39,134 @@ const routes: Routes = [
   },
   {
     path: 'animation-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'comedy-movies',
+    loadChildren: () => import('./pages/languages/english/genres/comedy-movies/comedy-movies.module').then( m => m.ComedyMoviesPageModule)
+  },
+  {
+    path: 'comedy-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'crime-movies',
+    loadChildren: () => import('./pages/languages/english/genres/crime-movies/crime-movies.module').then( m => m.CrimeMoviesPageModule)
+  },
+  {
+    path: 'crime-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'documentary-movies',
+    loadChildren: () => import('./pages/languages/english/genres/documentary-movies/documentary-movies.module').then( m => m.DocumentaryMoviesPageModule)
+  },
+  {
+    path: 'documentary-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'drama-movies',
+    loadChildren: () => import('./pages/languages/english/genres/drama-movies/drama-movies.module').then( m => m.DramaMoviesPageModule)
+  },
+  {
+    path: 'drama-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'family-movies',
+    loadChildren: () => import('./pages/languages/english/genres/family-movies/family-movies.module').then( m => m.FamilyMoviesPageModule)
+  },
+  {
+    path: 'family-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'fantasy-movies',
+    loadChildren: () => import('./pages/languages/english/genres/fantasy-movies/fantasy-movies.module').then( m => m.FantasyMoviesPageModule)
+  },
+  {
+    path: 'fantasy-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'history-movies',
+    loadChildren: () => import('./pages/languages/english/genres/history-movies/history-movies.module').then( m => m.HistoryMoviesPageModule)
+  },
+  {
+    path: 'history-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'horror-movies',
+    loadChildren: () => import('./pages/languages/english/genres/horror-movies/horror-movies.module').then( m => m.HorrorMoviesPageModule)
+  },
+  {
+    path: 'horror-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'music-movies',
+    loadChildren: () => import('./pages/languages/english/genres/music-movies/music-movies.module').then( m => m.MusicMoviesPageModule)
+  },
+  {
+    path: 'music-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'mystery-movies',
+    loadChildren: () => import('./pages/languages/english/genres/mystery-movies/mystery-movies.module').then( m => m.MysteryMoviesPageModule)
+  },
+  {
+    path: 'mystery-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'romance-movies',
+    loadChildren: () => import('./pages/languages/english/genres/romance-movies/romance-movies.module').then( m => m.RomanceMoviesPageModule)
+  },
+  {
+    path: 'romance-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'SF-movies',
+    loadChildren: () => import('./pages/languages/english/genres/SF-movies/SF-movies.module').then( m => m.SFMoviesPageModule)
+  },
+  {
+    path: 'SF-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'TV-movies',
+    loadChildren: () => import('./pages/languages/english/genres/TV-movies/TV-movies.module').then( m => m.TVMoviesPageModule)
+  },
+  {
+    path: 'TV-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'thriller-movies',
+    loadChildren: () => import('./pages/languages/english/genres/thriller-movies/thriller-movies.module').then( m => m.ThrillerMoviesPageModule)
+  },
+  {
+    path: 'thriller-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'war-movies',
+    loadChildren: () => import('./pages/languages/english/genres/war-movies/war-movies.module').then( m => m.WarMoviesPageModule)
+  },
+  {
+    path: 'war-movies/:id',
+    loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'western-movies',
+    loadChildren: () => import('./pages/languages/english/genres/western-movies/western-movies.module').then( m => m.WesternMoviesPageModule)
+  },
+  {
+    path: 'western-movies/:id',
     loadChildren: () => import('./pages/languages/english/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   },
   {
